58b945960fd53be04657ed79bbdab28c47ae420f
OmarFaig <omerfaiq7@gmail.com>
2024-01-05 20:55:51 +0100

    daily updates

--- Codes/bash_script.sh
+++ Codes/bash_script.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+
+# Set environment variables with your Git username and access token (or password)
+export GIT_USERNAME="OmarFaig"
+export GIT_TOKEN="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"
+
+# Perform Git operations
 git status
 git add .
 git commit -m "daily updates"
+
+# Use the environment variables for Git push
+git push -u origin master
+
 git push
-Username: OmarFaig
