239566aa4e41a43a35e49d02eff4d9520d1a4bcf
OmarFaig <omerfaiq7@gmail.com>
2024-01-06 21:00:29 +0100

    Seedgenerator

--- Codes/PointNet2.py
+++ Codes/PointNet2.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch import einsum
 import torch.nn.functional as F
 from pytorch3d.ops import sample_farthest_points, ball_query, knn_points
 from utils.pointnet_utils import index_points
@@ -14,6 +15,24 @@ def absolute_or_relative(value, total):
         return type(total)(value * total)
     else:
         raise ValueError(f"Absolute or relative value must be of type int or float, but is: {type(value)}")
+
+def grouping_operation(features, idx):
+    """
+    Parameters
+    ----------
+    features : torch.Tensor
+        (B, C, N) tensor of features to group
+    idx : torch.Tensor
+        (B, npoint, nsample) tensor containing the indicies of features to group with
+
+    Returns
+    -------
+    torch.Tensor
+        (B, C, npoint, nsample) tensor
+    """
+    return index_points(features.transpose(-2, -1), idx).movedim(-1, -3)
+
+
 class MLP_CONV(nn.Module):
     def __init__(self, in_channel, layer_dims, bn=None):
         super(MLP_CONV, self).__init__()
@@ -31,15 +50,49 @@ class MLP_CONV(nn.Module):
     def forward(self, inputs):
         return self.mlp(inputs)
 
+
+class MLP_Res(nn.Module):
+    def __init__(self, in_dim=128, hidden_dim=None, out_dim=128):
+        super(MLP_Res, self).__init__()
+        if hidden_dim is None:
+            hidden_dim = in_dim
+        self.conv_1 = nn.Conv1d(in_dim, hidden_dim, 1)
+        self.conv_2 = nn.Conv1d(hidden_dim, out_dim, 1)
+        self.conv_shortcut = nn.Conv1d(in_dim, out_dim, 1)
+
+    def forward(self, x):
+        """
+        Args:
+            x: (B, out_dim, n)
+        """
+        shortcut = self.conv_shortcut(x)
+        out = self.conv_2(torch.relu(self.conv_1(x))) + shortcut
+        return out
+
+
+
+
 class PointNet_SA_Layer(nn.Module):
     '''
     PointNet set of abstraction layer : sampling + grouping + PointNet layers
     based on paper : [1] PointNet++: Deep Hierarchical Feature Learning on Point Sets in a Metric Space - https://arxiv.org/abs/1706.02413
 
     '''
-    def __init__(self):
+    def __init__(self,in_channel,mlp_channels,*,npoints=None,nsample=None,radius=None,activation=F.relu()):
         super(PointNet_SA_Layer, self).__init__()
-        pass
+#activation relu ?!
+        self.npoints = npoints
+        self.nsample = nsample
+        self.radius = radius
+        self.activation = activation
+        self.convs = nn.ModuleList()
+        self.norms = nn.ModuleList()
+        last_channel = in_channel
+        for out_channel in mlp_channels[-1]:
+            self.convs.append(nn.Conv1d(last_channel, out_channel, 1))
+            self.norms.append(nn.BatchNorm1d())#nn.Identity()
+            last_channel = out_channel
+        self.last_conv = nn.Conv2d(last_channel,mlp_channels[-1],1 )
 
 
     def _sample(self,xyz,npoints):
@@ -85,9 +138,9 @@ class PointNet_SA_Layer(nn.Module):
                 grouped_xyz /= self.radius
             else:
                 #KNN
-            grouped_xyz = knn_points(sampled_xyz.transpose(-2,-1),xyz.transpose(-2,-1), K=self.nsample,return_sorted=False).idx
-            grouped_xyz = index_points(xyz.transpose(-2,-1),grouped_xyz).movedim(-1,-3)
-            grouped_xyz -=sampled_xyz.unsqueeze(-1)
+                grouped_xyz = knn_points(sampled_xyz.transpose(-2,-1),xyz.transpose(-2,-1), K=self.nsample,return_sorted=False).idx
+                grouped_xyz = index_points(xyz.transpose(-2,-1),grouped_xyz).movedim(-1,-3)
+                grouped_xyz -=sampled_xyz.unsqueeze(-1)
 
             if points is not None:
                 grouped_points = index_points(points.transpose(-2,-1),group_idx).movedim(-1,-3)
@@ -123,9 +176,77 @@ class PointNet_SA_Layer(nn.Module):
             if sampled_xyz is None:
                 sampled_xyz = self._sample(xyz, npoint)  # (B, C, S)
             grouped_points = self._group(xyz, points, sampled_xyz)  # (B, C+D, S, K)
+            for conv, norm in zip(self.convs, self.norms):
+                grouped_points = self.activation(norm(conv(grouped_points)))
+            grouped_points = self.last_conv(grouped_points)
+            new_points = torch.max(grouped_points, -1)[0]  # (B, D', S)
 
-            new_points = self.mlp_conv(grouped_points)
-            new_points = torch.max(new_points, 3)[0]
+            return sampled_xyz, new_points
 
-            return new_xyz, new_points
-        pass
+
+
+class vTransformer(nn.Module):
+    def __init__(self,
+                 in_channel,
+                 dim=256,
+                 n_knn=16,
+                 pos_hidden_dim=64,
+                 attn_hidden_multiplier=4):
+        super(vTransformer, self).__init__()
+        self.n_knn = n_knn
+        self.conv_key = nn.Conv1d(dim, dim, 1)
+        self.conv_query = nn.Conv1d(dim, dim, 1)
+        self.conv_value = nn.Conv1d(dim, dim, 1)
+
+        self.pos_mlp = nn.Sequential(nn.Conv2d(3, pos_hidden_dim, 1),
+                                     nn.BatchNorm2d(pos_hidden_dim), nn.ReLU(),
+                                     nn.Conv2d(pos_hidden_dim, dim, 1))
+
+        self.attn_mlp = nn.Sequential(
+            nn.Conv2d(dim, dim * attn_hidden_multiplier, 1),
+            nn.BatchNorm2d(dim * attn_hidden_multiplier), nn.ReLU(),
+            nn.Conv2d(dim * attn_hidden_multiplier, dim, 1))
+
+        self.linear_start = nn.Conv1d(in_channel, dim, 1)
+        self.linear_end = nn.Conv1d(dim, in_channel, 1)
+
+    def forward(self, x, pos):
+        """feed forward of transformer
+        Args:
+            x: Tensor of features, (B, in_channel, n)
+            pos: Tensor of positions, (B, 3, n)
+
+        Returns:
+            y: Tensor of features with attention, (B, in_channel, n)
+        """
+
+        identity = x
+
+        x = self.linear_start(x)
+        b, dim, n = x.shape
+
+        pos_flipped = pos.permute(0, 2, 1)#.contiguous()
+        _, idx_knn, _ = knn_points(pos_flipped, pos_flipped, K=self.n_knn, return_sorted=False)
+        key = self.conv_key(x)  # (B, dim, N)
+        value = self.conv_value(x)
+        query = self.conv_query(x)
+
+        key = grouping_operation(key, idx_knn)  # (B, dim, N, k)
+        qk_rel = query.reshape((b, -1, n, 1)) - key
+
+        pos_rel = pos.reshape(
+            (b, -1, n, 1)) - grouping_operation(pos, idx_knn)  # (B, 3, N, k)
+        pos_embedding = self.pos_mlp(pos_rel)  # (B, dim, N, k)
+
+        attention = self.attn_mlp(qk_rel + pos_embedding)
+        attention = torch.softmax(attention, -1)
+
+        # knn value is correct
+        value = grouping_operation(value,
+                                   idx_knn) + pos_embedding  # (B, dim, N, k)
+
+        agg = einsum('b c i j, b c i j -> b c i', attention,
+                     value)  # (B, dim, N)
+        y = self.linear_end(agg)  # (B, in_dim, N)
+
+        return y + identity
--- Codes/model.py
+++ Codes/model.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 from torch import einsum
 import warnings
 import logging
+from PointNet2 import MLP_Res,MLP_CONV, PointNet_SA_Layer,vTransformer
 logger = logging.getLogger(__name__)
 
 
@@ -12,3 +13,30 @@ class FeatureExtractor(nn.Module):
         pass
 
 
+
+class SeedGenerator(nn.Module):
+    def __init__(self, feat_dim=512, seed_dim=128, n_knn=20, factor=2, attn_channel=True):
+        super(SeedGenerator, self).__init__()
+        self.uptrans = UpTransformer(256, 128, dim=64, n_knn=n_knn, use_upfeat=False, attn_channel=attn_channel, up_factor=factor, scale_layer=None)
+        self.mlp_1 = MLP_Res(in_dim=feat_dim + 128, hidden_dim=128, out_dim=128)
+        self.mlp_2 = MLP_Res(in_dim=128, hidden_dim=64, out_dim=128)
+        self.mlp_3 = MLP_Res(in_dim=feat_dim + 128, hidden_dim=128, out_dim=seed_dim)
+        self.mlp_4 = nn.Sequential(
+            nn.Conv1d(seed_dim, 64, 1),
+            nn.ReLU(),
+            nn.Conv1d(64, 3, 1)
+        )
+
+    def forward(self, feat, patch_xyz, patch_feat):
+        """
+        Args:
+            feat: Tensor (B, feat_dim, 1)
+            patch_xyz: (B, 3, 128)
+            patch_feat: (B, seed_dim, 128)
+        """
+        x1 = self.uptrans(patch_xyz, patch_feat, patch_feat, upfeat=None)  # (B, 128, 256)
+        x1 = self.mlp_1(torch.cat([x1, feat.repeat((1, 1, x1.size(2)))], 1))
+        x2 = self.mlp_2(x1)
+        x3 = self.mlp_3(torch.cat([x2, feat.repeat((1, 1, x2.size(2)))], 1))  # (B, 128, 256)
+        completion = self.mlp_4(x3)  # (B, 3, 256)
+        return completion, x3
