31407e18d9a189f76fc64173d16daeb5e2c674c0
OmarFaig <omerfaiq7@gmail.com>
2024-02-18 22:10:19 +0100

    pad 1000 points - bigger model

--- Codes/utils/dataloader.py
+++ Codes/utils/dataloader.py
@@ -32,12 +32,28 @@ class RacingDataset(Dataset):
             if len(points)>=50:
                 filtered_list.append(filename)
         return filtered_list
-#dataset = RacingDataset(root_dir="/home/omar/TUM/Data/cropped/sim")
-#dataloader = DataLoader(dataset,batch_size=1,shuffle=False,num_workers=2)
+
+    def get_largest_point_cloud(self):
+        largest_pcd_path = None
+        largest_num_points = 0
+        for filename in self.filter_file_list:
+            pcd = o3d.io.read_point_cloud(os.path.join(self.root_dir, filename))
+            num_points = len(pcd.points)
+            if num_points > largest_num_points:
+                largest_num_points = num_points
+                largest_pcd_path = os.path.join(self.root_dir, filename)
+        print("Largest point cloudl", largest_num_points)
+        print("Largest point cloudl", largest_pcd_path)
+
+        return largest_pcd_path, largest_num_points
+dataset = RacingDataset(root_dir="/home/omar/TUM/Data/cropped/sim")
+dataset.get_largest_point_cloud()
+dataloader = DataLoader(dataset,batch_size=1,shuffle=False,num_workers=2)
 
 #for i in dataset:
 #    #pcd = o3d.io.read_point_cloud(i)
 #    print(i.shape)
-#
+
 #print(len(dataloader))
+
 #
