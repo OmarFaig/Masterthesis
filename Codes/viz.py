e3f0201590e4d5833d3afccbe194f7dd0f74c64a
OmarFaig <omerfaiq7@gmail.com>
2024-06-16 11:13:40 +0200

    viz documentation

--- Codes/viz.py
+++ Codes/viz.py
@@ -11,32 +11,19 @@ pc_folder_path = '/home/omar/TUM/Data/SeedFormer_2602_npy/reconstructed_2205v002
 # Path to folder containing bounding box text files
 bbox_folder_path = '/home/omar/TUM/Data/SeedFormer_2602_npy/reconstructed_1105v004/labels'
 
-# List all .npy files in the folder
-file_list = sorted([f for f in os.listdir(pc_folder_path) if f.endswith(('.npy','.pcd'))])
+# List all .npy and .pcd files in the folder
+file_list = sorted([f for f in os.listdir(pc_folder_path) if f.endswith(('.npy', '.pcd'))])
 
 print(file_list)
+
 # Create Open3D visualizer
 vis = o3d.visualization.VisualizerWithKeyCallback()
 vis.create_window()
 
+# Set rendering options
 vis.get_render_option().point_size = 3.0
 vis.get_render_option().background_color = np.zeros(3)
-#view_control = vis.get_view_control()
 
-#view_control.set_up([0, 1, 0])
-#ctr = vis.get_view_control()
-#parameters = ctr.convert_to_pinhole_camera_parameters()
-#parameters.extrinsic = np.array([[1, 0, 0, 0],
-                          #      [1, 0, 0, 0],  # Look from the side, flipping y-axis
-                          #      [1, 0, 0, 0],
-                          #      [1, 0, 0, 0]])  # Translation remains unchanged
-#ctr.convert_from_pinhole_camera_parameters(parameters)
-# Lock the view up direction along the z-axis
-
-# view_control.set_lookat(bbox.get_center())
-#
-#view_control.set_lookat(np.array([7.0, 8.0, 100.0]))
-#view_control.scale(9)  # Adjust the zoom level as needed to zoom in closer
 # Function to load .npy point cloud and bounding box coordinates
 def load_data(pc_folder, bbox_folder, file_name):
     if file_name.endswith('.npy'):
@@ -49,23 +36,17 @@ def load_data(pc_folder, bbox_folder, file_name):
         pcd = o3d.io.read_point_cloud(pcd_path)
         point_cloud = np.asarray(pcd.points)
 
-
+    # Load bounding box coordinates from corresponding text file
     bbox_file_path = os.path.join(bbox_folder, file_name.replace('.npy', '.txt').replace('.pcd', '.txt'))
     print(bbox_file_path)
 
     with open(bbox_file_path, 'r') as file:
-        line = file.readline().strip()  # Read the single line from the file
-        bbox_coordinates = line.split()  # Split the line into label and coordinates
-        print(bbox_coordinates)
-        # Check if the label is "Car", if yes, discard it
-
+        line = file.readline().strip()
+        bbox_coordinates = line.split()
         bbox_coordinates = bbox_coordinates[:-1]  # Exclude the label
-
-        bbox_coordinates = list(map(float, bbox_coordinates))  # Convert coordinates to float and then to NumPy array
-
+        bbox_coordinates = list(map(float, bbox_coordinates))  # Convert coordinates to float
 
     return point_cloud, bbox_coordinates
-
 
 # Function to visualize point cloud and bounding box
 def visualize(point_cloud, bbox_coordinates):
@@ -77,8 +58,8 @@ def visualize(point_cloud, bbox_coordinates):
     bbox = None
     if bbox_coordinates is not None:
         bbox = o3d.geometry.OrientedBoundingBox(center=bbox_coordinates[:3],
-                                                 R=np.eye(3),
-                                                 extent=bbox_coordinates[3:6])
+                                                R=np.eye(3),
+                                                extent=bbox_coordinates[3:6])
         bbox.color = [1, 0, 0]  # Set bbox color to red
 
     # Add geometries to the visualizer
@@ -86,52 +67,27 @@ def visualize(point_cloud, bbox_coordinates):
     vis.add_geometry(pcl)
     if bbox:
         vis.add_geometry(bbox)
-    # Get the view control
-    view_control = vis.get_view_control()
 
+    # Get the view control and set view parameters
+    view_control = vis.get_view_control()
     view_control.set_front([0.1, -0.5, 0.6])
     view_control.set_lookat(bbox.get_center())
-    #view_control.set_up([-0.36828927493940194, 0.49961995188329117, 0.78405542766104697])
     view_control.set_zoom(0.009)
-    # Set camera parameters
-    #if bbox:
-    #    bbox_center = bbox.get_center()
 
-    #    # Calculate the extent of the bounding box
-    #    bbox_points = np.asarray(bbox.get_box_points())
-    #    bbox_min = np.min(bbox_points, axis=0)
-    #    bbox_max = np.max(bbox_points, axis=0)
-    #    bbox_extent = bbox_max - bbox_min
-
-    #    # Calculate the position 1 meter away from the bounding box in the direction of the top right corner
-    #    camera_pos = np.array(bbox_center) + np.array([bbox_extent[0] / 2, bbox_extent[1] / 2, 1.0])
-
-    #    # Look at the center of the bounding box
-    #    view_control.set_lookat(bbox_center)
-
-    #    # Set camera position
-    #    view_control.set_front(camera_pos)
-    #    view_control.set_up([0, 0, 1])  # Set the up direction to be the z-axis
-
-    #    # Adjust zoom level
-
-    #    view_control.set_zoom(0.05)  # Adjust the zoom level as needed
-
+# Function to load the next frame
 def load_next_frame(vis):
     global frame_index
     print("Loading frame", frame_index)
 
     vis.clear_geometries()
-    # Set the camera view to look at the center of the bounding box
 
     if frame_index < len(file_list):
         point_cloud, bbox_coordinates = load_data(pc_folder_path, bbox_folder_path, file_list[frame_index])
         if point_cloud is not None:
             visualize(point_cloud, bbox_coordinates)
         frame_index += 1
-    #view_control.set_lookat(bbox_coordinates[:3])
 
-
+# Function to load the previous frame
 def load_prev_frame(vis):
     global frame_index
     if frame_index >= 2:
@@ -141,20 +97,22 @@ def load_prev_frame(vis):
         frame_index = 0
         return load_next_frame(vis)
 
+# Function to close the visualizer
 def close_vis(vis):
     vis.close()
     vis.destroy_window()
 
-
-
 # Register key callbacks
-key_to_callback = {}
-key_to_callback[ord("N")] = load_next_frame
-key_to_callback[ord("B")] = load_prev_frame
-key_to_callback[ord("X")] = close_vis
+key_to_callback = {
+    ord("N"): load_next_frame,
+    ord("B"): load_prev_frame,
+    ord("X"): close_vis
+}
 
+# Load the first frame
 load_next_frame(vis)
 
+# Register callbacks for each key
 for key, val in key_to_callback.items():
     vis.register_key_callback(key, val)
 
